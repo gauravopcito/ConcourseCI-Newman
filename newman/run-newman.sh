92c1d97f89dcb5a6806ccef9b4bec57b1e6c0a29
gauravopcito <gaurav.dabhade@opcito.com>
2020-04-01 18:18:32 +0530

    Updated pipeline

--- newman/run-newman.sh
+++ newman/run-newman.sh
@@ -14,15 +14,15 @@ npm install newman --global --no-spin
 
 echo "New Newman Version: " `newman --version`
 
-server = `jq '.values[1].value' $ENVIRONMENT_URL
+server=`jq '.values[1].value' $ENVIRONMENT_URL`
 echo "Server: " $server
-port = `jq '.values[2].value' $ENVIRONMENT_URL
+port=`jq '.values[2].value' $ENVIRONMENT_URL`
 echo "Port: " $port
-host_url = "http://"$server":"$port"/auth/login"
+host_url="http://"$server":"$port"/auth/login"
 echo "hosturl: " $host_url
 basic_login_curl=$(curl -w "200" -s -X POST -d "username=gauravdabhade24@gmail.com&password=password" "$host_url")
 echo "login response: " $basic_login_curl
-auth_token = echo $basic_login_curl | jq '.auth_token'
+auth_token=echo $basic_login_curl | jq '.auth_token'
 echo "auth_token: " $auth_token
 sed -i "s|TOKEN|$auth_token|g" $GLOBAL_URL
 
